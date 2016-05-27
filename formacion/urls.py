a3f49a7312f1e03f7f112e52c8779037e0d7f126
Instituto Europeo de Posgrado - Dpto. de Tecnología - Jose Maria Fernández  Campos <tecnologia.jf@iep.edu.es>
2016-05-27 17:48:09 +0200

    Static files not found. Trying to solve this issue

--- formacion/urls.py
+++ formacion/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+from faq import views
+
 urlpatterns = [
+	url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
 	url(r'^faq/', include('faq.urls', namespace="faq"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
