cd153b6ceaf49eb884445cf296857b0a4270e4d0
Instituto Europeo de Posgrado - Dpto. de Tecnología - Jose Maria Fernández  Campos <tecnologia.jf@iep.edu.es>
2016-05-30 09:58:17 +0200

    Trying to solve staticfiles issue

--- formacion/settings.py
+++ formacion/settings.py
@@ -128,7 +128,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static'), # PROJECT_ROOT
-)
